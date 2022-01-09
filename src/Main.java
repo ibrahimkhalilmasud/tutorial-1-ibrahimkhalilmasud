ae2d897125edac49d2e3996ecdc25e697caa8b1d
Emma Paris <ibrahimkhalilmasud@icloud.com>
2022-01-09 11:09:48 +0800

    Done 2

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    private in age;
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int age = 50;
+    }
+}
+}
+//
+//class Main {
+//
+//    public static void main(String[] args)
+//    {
+//
+//        System.out.println("Helloworld");
+//
+//    }
+//
+//}
--- src/OddNumbers.java
+++ src/OddNumbers.java
@@ -11,3 +11,13 @@ public class OddNumbers {
         }
     }
 }
+
+
+//    public static void main(String[] args) {
+//        int N;
+//        N = 1;
+//        while (N <= 32) {
+//            N = 2 * N;
+//            System.out.println(N);
+//        }
+//    }
